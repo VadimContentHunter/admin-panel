79d329c6d0c7c6a31369b058e133e4b1ab6530b1
Vadim <project.k.vadim@gmail.com>
2023-02-10 16:40:33 +0200

    environment setup

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\AdminPane\tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class ApplicationTest extends TestCase
+{
+}
