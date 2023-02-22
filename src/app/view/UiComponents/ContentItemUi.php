75dbb7e030a332355dadad4af7c542e18ad63ed7
Vadim <project.k.vadim@gmail.com>
2023-02-22 03:03:40 +0200

    create ContentItemUi class

---
+++ src/app/view/UiComponents/ContentItemUi.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\AdminPane\view\UiComponents;
+
+use vadimcontenthunter\AdminPane\view\interfaces\IContentItemUi;
+use vadimcontenthunter\AdminPane\view\interfaces\IBaseUiComponent;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class ContentItemUi implements IBaseUiComponent, IContentItemUi
+{
+    public function __construct(
+        protected string $title,
+        protected string $description = ''
+    ) {
+    }
+
+    public function getGridColumnCount(): int
+    {
+        return 3;
+    }
+
+    public function getHtml(): string
+    {
+        return <<<HTML
+            <section class="container-base-style container-one">
+                <div class="panel-header">
+                    <h4>Личные данные</h4>
+                    <a class="icon-panel">
+                        <i></i>
+                    </a>
+                </div>
+                <form>
+                    <ul>
+                        <li>
+                            <label >Имя пользователя</label>
+                            <input type="text" name="login" placeholder="Введите имя пользователя" value="Вадим Б." readonly>
+                            <output name="result_name">Некорректно указан логин.</output>
+                        </li>
+                        <li>
+                            <label >Электронная почта</label>
+                            <input type="email" name="email" placeholder="Введите электронную почту" value="vadim.test.1@mail.com" readonly>
+                            <output name="result_name">Неверно указанная почта.</output>
+                        </li>
+                        <li>
+                            <label >Пароль</label>
+                            <input type="password" name="password" placeholder="Введите пароль" value="************" readonly>
+                            <output name="result_name">Некорректно указан пароль.</output>
+                        </li>
+                        <li class="hidden">
+                            <label >Повторите пароль</label>
+                            <input type="password" name="confirm_password" placeholder="Повторите пароль">
+                            <output name="result_name">Пароли не совпадают.</output>
+                        </li>
+                    </ul>
+                    <div class="panel-footer">
+                        <button type="button">Сохранить</button>
+                    </div>
+                </form>
+            </section>
+        HTML;
+    }
+}
