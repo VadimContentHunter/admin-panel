48465abbcc220a1a87a22df235623d0be610baa6
Vadim <project.k.vadim@gmail.com>
2023-03-27 07:30:59 +0300

    created implementation IControlItemUi

---
+++ src/views/UiComponents/Header/ControlItemUi.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\AdminPanel\views\UiComponents\Header;
+
+use vadimcontenthunter\AdminPanel\views\UiComponents\Header\interfaces\IControlItemUi;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class ControlItemUi implements IControlItemUi
+{
+    public function getHtml(): string
+    {
+        return '';
+    }
+}
