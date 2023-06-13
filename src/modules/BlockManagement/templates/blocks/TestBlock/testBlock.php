15cafb6eae135fd0b3ec44cf9221848e83c88ff9
Vadim <project.k.vadim@gmail.com>
2023-06-13 20:54:02 +0300

    added block for BlockManagement module

---
+++ src/modules/BlockManagement/templates/blocks/TestBlock/testBlock.php
@@ -0,0 +1,33 @@
+<section class="container-main-screen">
+    <span class="img-1"></span>
+    <div class="wrapper">
+        <div class="content">
+            <p class="main-text">Надійна автошкола, що тримає провідні позиції серед автошкіл Києва</p>
+            <h2 class="title">Ми вчимо, поки не навчимо!</h2>
+            <form action="db8d4797b0e5e482bb868f0926148e8f/form/feedback" json-rpc-method="formPhone">
+                <input type="tel" name="tel" placeholder="+38(0_ _)_ _ _-_ _ _ _">
+                <input type="button" value="Отримати консультацію">
+            </form>
+            <small class="additional-information">Натискаючи кнопку, Ви даєте згоду на обробку персональних даних. Натисніть для детальної інформації,                <a href="">Умова обробки особистих даних. . .</a>
+            </small>
+            <section class="social-m">
+                <h4>Підписуйтесь на нас</h4>
+                <menu>
+                    <li>
+                        <a href="" class="icon-youtube"><i></i></a>
+                    </li>
+                    <li>
+                        <a href="" class="icon-inst"><i></i></a>
+                    </li>
+                    <li>
+                        <a href="" class="icon-telegram"><i></i></a>
+                    </li>
+                    <li>
+                        <a href="" class="icon-facebook"><i></i></a>
+                    </li>
+                </menu>
+            </section>
+        </div>
+        <span class="img-2"></span>
+    </div>
+</section>
